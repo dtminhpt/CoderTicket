66a6b66d03d086c779216c4c8aab9a79eed77040
Minh Dinh <dtminhpt@gmail.com>
2016-03-26 15:34:48 +0700

    Testing validations of category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+  let(:category) { category = Category.new }
+
   context "validations" do
     it "checks for presence of name" do
-      category = Category.new
-      expect(category.save).to eq false
+      category.save
+      expect(category.errors).to_not be_empty
+      expect(category.errors[:name]).to include "is not included in the list"
     end
   end
 end
