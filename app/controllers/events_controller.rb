72f24655b3769cc6d03eab69b4919ef593aa19e3
Minh Dinh <dtminhpt@gmail.com>
2016-03-28 11:09:53 +0700

    User can create an event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     
     if params[:keyword].present?
@@ -12,4 +14,27 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def new
+    @event = Event.new 
+  end 
+
+  
+
+  def create
+    @event = current_user.events.new(event_params)
+
+    if @event.save
+      flash[:success] = 'The event has been created successfully.'
+      redirect_to root_path
+    else
+      render :new
+    end 
+  end 
+
+  private 
+    def event_params
+      params.require(:event).permit(:category_id, :venue_id, :name, :extended_html_description, :hero_image_url, :starts_at, :ends_at)
+    end 
+
 end
