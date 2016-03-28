2c37081aeb1409fdc99a8e97874e088d69dce565
Minh Dinh <dtminhpt@gmail.com>
2016-03-28 19:20:52 +0700

    Fix bug related to tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -8,14 +8,14 @@ class TicketsController < ApplicationController
       flash[:warning] = "You cannot buy tickets of past events."
       redirect_to event_path(@event)
     else
-      @ticket = Ticket.where(user: current_user, event: @event)
+      @tickets = Ticket.where(user: current_user, event: @event)
     end 
   end
 
   def create
-    @event = Event.find(params[:user_id])
-    params[:type_quantities].each do |type, quantity|
-      Ticket.create!(user: current_user, event: @event, ticket_type_id: type, quantity: quantity)
+    @event = Event.find(params[:event_id])
+    params[:type_quantities].each do |type_id, quantity|
+      Ticket.create!(user: current_user, event: @event, ticket_type_id: type_id, quantity: quantity) if quantity.to_i > 0
     end
     flash[:success] = "Thank you for buying the event tickets."
     redirect_to new_event_ticket_path(@event)
